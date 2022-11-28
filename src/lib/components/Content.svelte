905ca6f8f322dc4222ed384cb68a2c347dd971c7
Mardax <62964422+mardax007@users.noreply.github.com>
2022-11-28 14:00:12 +0100

    feat: :construction: ground work

---
+++ src/lib/components/Content.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let title: string;
+	export let text: string;
+	export let imageURL: string;
+	export let alignment: "left" | "right" = "right";
+</script>
+
+<div id={title} class={alignment}>
+	<div id="content">
+		<h1>{title}</h1>
+		<p>{text}</p>
+	</div>
+	<div id="visual">
+		<img src={imageURL} alt={`${title} image`} />
+	</div>
+</div>
+
+<style lang="scss">
+	.right {
+		margin-left: 5%;
+		margin-right: 5%;
+		
+		height: 55vh;
+
+		font-family: "Poppins", "sans-serif";
+
+		#content {
+			float: left;
+			width: 55%;
+
+			h1 {
+				font-size: calc((1.3vw + 1vh) / 0.4375);
+			}
+
+			p {
+				font-size: calc(1.3vw + 1vh);
+			}
+		}
+
+		#visual {
+			float: right;
+			width: 45%;
+			height: 100%;
+			background-color: yellow;
+
+			img {
+				width: 100%;
+			}
+		}
+
+		background-color: blue;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+	import "../global.scss";
+	import Content from '$lib/components/Content.svelte';
+
+</script>
+
+<div id="wrapper">
+	<Content title="Test" text="Duurzame en langlevende producten maken is de beste manier om minder te produceren. De armaturen die we in onze jonge jaren hebben gemaakt draaien nog steeds in top conditie. Hoge kwaliteit LED-chips functioneren rond 20 jaar. Dankzij ons ontwerp zijn onderdelen die vervangen moeten worden makkelijk te vervangen. Wij kunnen gloei- en halogeen armaturen en lampen ombouwen naar LED. Onze hoge kwaliteit onderdelen zijn onze grootste bijdrage aan het milieu." imageURL="https://p.mardax.tech/images/v1.png" />
+</div>
+
+<style lang="scss">
+	#wrapper {
+		background-color: red;
+		height: 100vh;
+		width: 100vw;
+		
+	}
+</style>
