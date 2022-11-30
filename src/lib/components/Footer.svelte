00d59f661ef6ac7a9f4d7ba4da0205706d425953
Mardax <62964422+mardax007@users.noreply.github.com>
2022-11-30 13:49:38 +0100

    feat: :sparkles: Added footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,76 @@
+
+<div id="footer">
+    <h1>Contact</h1>
+    <div id="info">
+        <div id="contact">
+            <h2>PROLIAD LED LIGHTING</h2>
+            <p>Vrijkenstraat 18, 6088 PA, Roggel</p>
+            <p>info@proliad.com</p>
+            <p>+31 (0)4 75 44 44 20</p>
+        </div>
+        <div id="socials">
+            <img src="https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Fproliad-facebook.png?alt=media" alt="facebook" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Fproliad-twitter.png?alt=media" alt="facebook" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Fproliad-linkedin.png?alt=media" alt="facebook" />
+            <p>Schrijf je <a href="https://www.proliad.com/nieuws">hier</a> in voor onze nieuwsbrief.</p>
+            <p>Open van maandag tot en met vrijdag tussen 09.00 en 17.00 uur. Bezoek geschiedt alléén op afspraak.</p>
+        
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+    * {
+        background-color: #f78f1e;
+        color: #fdf8ff;
+    }
+    #footer {
+        width: 100vw;
+        margin-top: calc(-75px - 2.5vw);
+        padding-top: calc(1vh + 1vw);
+
+        h1 {
+            text-align: center;
+            font-size: calc(2vw + 2.5vh);
+        }
+
+        #info {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            margin-top: calc(1vw + 1vh);
+            margin-left: 5vw;
+            padding-bottom: calc(1.5vh + 1vw);
+
+            #contact {
+                margin-left: 2vw;
+                h2 {
+                    font-size: calc(1.5vw + 1.5vh);
+                }
+
+                p {
+                    font-size: calc(1vw + 0.75vh);
+                    line-height: calc(1vw + 2vh);
+                    max-width: 35vw;
+                }
+            }
+
+            #socials {
+                margin-right: 2vw;
+                margin-top: calc(1.5vw + 1.5vh);
+
+                p {
+                    font-size: calc(1vw + 0.50vh);
+                    max-width: calc(49vw - 2.5vh);
+                }
+
+                img {
+                    width: calc(1.5vw + 2vh);
+                    height: auto;
+                    margin-top: 1vh;
+                    margin-right: calc((1.5vw + 2vh) * 0.5);
+                    margin-bottom: 1vh;
+                }
+            }
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
 
 	import { firestore } from '$lib/scripts/firebaseControl';
 	import { onMount } from "svelte";
+	import Footer from "$lib/components/Footer.svelte";
 
 	let contentItems: {[key: string]: DocumentData} = { "Kwaliteit": { "title": "Kwaliteit", "text": "Duurzame en langlevende producten maken is de beste manier om minder te produceren. De armaturen die we in onze jonge jaren hebben gemaakt draaien nog steeds in top conditie. Hoge kwaliteit LED-chips functioneren rond 20 jaar. Dankzij ons ontwerp zijn onderdelen die vervangen moeten worden makkelijk te vervangen. Wij kunnen gloei- en halogeen armaturen en lampen ombouwen naar LED. Onze hoge kwaliteit onderdelen zijn onze grootste bijdrage aan het milieu.", "imageURL": "https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Flamp.svg?alt=media" }, "Lokale productie": { "text": "Nederlandse productkwaliteit staat hoog aangeschreven in de wereld. We produceren onze led armaturen te Roggel in onze eigen fabriek. Door lokale productie hoeven wij geen voorraad aan te houden, zodra er armaturen worden besteld kunnen wij die meteen produceren. Wij gebruiken aluminium vaak omdat wij deze gemakkelijk kunnen hergebruiken. Door met een vaste groep ZZP’ers voor de productie te werken kunnen we constante kwaliteit bewaken.", "imageURL": "https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Fwinkel.svg?alt=media", "title": "Lokale productie" }, "Refurbished LED producten": { "imageURL": "https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Fdozen.svg?alt=media", "text": "Onze LED producten die we 10-15 jaar geleverd hebben zijn nog steeds refurbushbaar. Door de LED en voeding uit te wisselen en mogelijk de optiek aan te passen geef je een 10-15 jaar oude LED nog 10-15 jaar aan levensduur. Proliad refurbished zelfs goede armaturen die we zelf niet geleverd hebben, indien dit product zich ervoor verleent. Het produceren van de behuizing, die voornamelijk bestaat uit aluminium, kost de meeste energie.", "title": "Refurbished LED producten" }, "Verpakken": { "text": "Betreft de verpakking investeren we in bakken die van gerecycled plastic zijn gemaakt. Het blijft onze wel bekende transportbak met statiegeld erop. Het vouwen van dozen is hierdoor verleden tijd. De welbekende doos zullen we altijd blijven recyclen als het kan inclusief het verpakkingsmateriaal. Tevens is er geïnvesteerd in papieren plakband en plastic zakken op mais basis.", "imageURL": "https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Frecyclebak.svg?alt=media", "title": "Verpakken" }, "Vervoeren": { "title": "Vervoeren", "imageURL": "https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Fbusje.svg?alt=media", "text": "Wij proberen onze productie zo lokaal mogelijk te maken, dit is goed voor het milieu en bespaart logistieke kosten. Daar waar dat wel nodig is, zoals bij enkele LED chips of drivers, selecteren wij onze leveranciers mede op hun milieubeleid en maatregelen. Ons eigen transport proberen we zo veel mogelijk elektrisch te doen, recent hebben we gekozen voor een extra aanhanger i.p.v. een extra bus." }, "Huisvesting": { "text": "Proliad heeft geïnvesteerd in een nieuw bedrijfspand dat voorzien is van zonnepanelen om de bedrijfsactiviteiten van stroom te voorzien, verder hebben we een warmtepomp geïnstalleerd om het geheel te verwarmen. Proliad zal groen blijven produceren en altijd zijn best doen om stap voor stap de wereld groener te maken.", "imageURL": "https://firebasestorage.googleapis.com/v0/b/proliad.appspot.com/o/assets%2Fhuisvesting.svg?alt=media", "title": "Huisvesting" } };;
 
@@ -37,14 +38,14 @@
 </script>
 
 <div id="wrapper">
-	{#key contentItems && other}
-		<Navbar logoURL={other?.logoURL ?? "/logo.png"} />
-		<img id="wereldbol" src={other.globeURL ?? "/wereldbol.svg"} alt="wereldbol" />
-		<Tagline text={other.tagline ?? ""} />
-		<Intro text={other.intro  ?? ""} />
-		<Content {contentItems} order={other.order} />
-	{/key}
+	<Navbar logoURL={other?.logoURL ?? "/logo.png"} />
+	<img id="wereldbol" src={other.globeURL ?? "/wereldbol.svg"} alt="wereldbol" />
+	<Tagline text={other.tagline ?? ""} />
+	<Intro text={other.intro  ?? ""} />
+	<Content {contentItems} order={other.order} />
+	<Footer />
 </div>
 
 <style lang="scss">
+	
 </style>
